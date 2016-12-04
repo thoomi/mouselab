a8a45974f8c5392e026ab9ad731953657d070f34
Thomas <thomi-blank@gmx.de>
2016-12-04 13:40:46 +0100

    Updated time costs

--- frontend/app/scripts/controllers/step10.js
+++ frontend/app/scripts/controllers/step10.js
@@ -10,10 +10,10 @@
 angular.module('mouselabApp')
   .controller('Step10Ctrl', function ($scope, $location,$interval, dataService, configData) {
       if (!dataService.everythingIsValid()) { $location.path(''); }
-      
+
       dataService.incrementSiteNumber();
       dataService.initializeTrials();
-      
+
       $scope.cueLabels     = configData.getCueLabels();
       $scope.cueValues     = configData.getCueValues();
       $scope.currentRound  = dataService.getCurrentRound();
@@ -23,9 +23,9 @@ angular.module('mouselabApp')
       $scope.currentTrial  = dataService.getNextTrial();
       $scope.maxScore      = configData.getMaxScore(dataService.getCurrentTask());
       $scope.taskWaiting   = configData.isTaskWithoutWaiting(dataService.getCurrentTask());
-      
+
       $scope.experimentCondition = dataService.getParticipantCondition();
-      
+
       $scope.maxTrials           = configData.getMaxPossibleTrials(dataService.getCurrentTask());
       $scope.finishedTrials      = 0;
       $scope.currentScore        = 0;
@@ -35,15 +35,15 @@ angular.module('mouselabApp')
       $scope.finishedTrialData   = [];
       $scope.savingInProgress    = false;
       $scope.sumOfTimeCosts      = 0;
-      
+
       $scope.timeOfLastAcquiredTrial = configData.getAvailableTime(dataService.getCurrentTask());
-      
-      
+
+
       var countDownStepValue = 10;
       $scope.currentCueTime  = 0;
       $scope.currentCueEndTime = 0;
-      
-      
+
+
       $scope.showCueValues  = [
         {
           id: '1',
@@ -73,36 +73,36 @@ angular.module('mouselabApp')
           intervalId: -1,
           countdownTime: $scope.cueValues[3].cost
         }];
-      
-      
-      
-      
+
+
+
+
       function saveExperiment() {
           if ($scope.savingInProgress) { return; }
-          
+
           var timeToFinish = configData.getAvailableTime(dataService.getCurrentTask());
-          
+
           if ($scope.availableTime > 0)
           {
             timeToFinish = configData.getAvailableTime(dataService.getCurrentTask()) - $scope.availableTime;
           }
-          
+
           // Cancel all running intervals
           $interval.cancel(intervalId);
-          
+
           angular.forEach($scope.showCueValues, function(value, key) {
             $interval.cancel(value.intervalId);
           });
-        
-          
+
+
           $scope.availableTime    = 0;
           $scope.remainingSeconds = 0;
           $scope.remainingMinutes = 0;
-          
+
           $scope.savingInProgress = true;
           $scope.buyTimerRunning  = true;
           $scope.informationAcquired = false;
-          
+
           dataService.saveExperiment($scope.finishedTrialData, timeToFinish, $scope.currentScore,  function(error){
             if (!error)
             {
@@ -115,8 +115,8 @@ angular.module('mouselabApp')
             }
           });
         }
-        
-        
+
+
       function setupNextTrial() {
         $scope.timeOfLastAcquiredTrial = $scope.availableTime;
         $scope.finishedTrials++;
@@ -125,7 +125,7 @@ angular.module('mouselabApp')
         $scope.aquiredInfos        = [];
         $scope.currentTrial        = dataService.getNextTrial();
         $scope.sumOfTimeCosts      = 0;
-        
+
         angular.forEach($scope.showCueValues, function(value, key) {
           value.show = false;
           value.isAcquired = false;
@@ -133,7 +133,7 @@ angular.module('mouselabApp')
           value.intervalId = -1;
           value.countdownTime = $scope.cueValues[key].cost;
         });
-        
+
         // All trials done
         if ($scope.finishedTrials === $scope.maxTrials)
         {
@@ -152,74 +152,74 @@ angular.module('mouselabApp')
           saveExperiment();
           return;
         }
-    
+
         $scope.availableTime -= countDownStepValue;
-        
+
         if ($scope.currentCueTime > 0)
         {
           $scope.currentCueTime -= countDownStepValue;
         }
-        
+
         $scope.remainingMinutes = Math.floor(($scope.availableTime / (1000.0 * 60.0)) % 60);
         $scope.remainingSeconds = ($scope.availableTime / 1000.0) % 60;
-    
+
       }, 10);
-  
-  
+
+
     $scope.$on('$destroy', function() {
       // Make sure that the interval is destroyed too
       $interval.cancel(intervalId);
-      
+
       angular.forEach($scope.showCueValues, function(value, key) {
         $interval.cancel(value.intervalId);
       });
     });
-  
-    
+
+
     $scope.buyCue = function(index) {
-      
+
       if ($scope.showCueValues[index].intervalId !== -1 || $scope.buyTimerRunning) { return;  }
-      
+
       $scope.informationAcquired = false;
-      
+
       $scope.buyTimerRunning = true;
-      
+
       if ($scope.taskWaiting)
       {
         $scope.showCueValues[index].isAcquired = true;
-        
+
         var acquisitionPattern = determineAcquisitionPattern();
         var timeCost = determineTimeCost(acquisitionPattern, dataService.getCurrentTask());
-        
+
         var localTimeCost = timeCost - $scope.sumOfTimeCosts;
-        
+
         var countDownSteps = $scope.showCueValues[index].countdownTime / 10;
         countDownStepValue = ($scope.showCueValues[index].countdownTime + localTimeCost) / countDownSteps;
-        
+
         //$scope.showCueValues[index].countdownTime += localTimeCost;
-        
+
         $scope.sumOfTimeCosts = timeCost;
         $scope.currentCueTime  = $scope.showCueValues[index].countdownTime + localTimeCost;
         $scope.currentCueEndTime = $scope.availableTime - ($scope.showCueValues[index].countdownTime + localTimeCost);
-        
+
         $scope.showCueValues[index].intervalId = $interval(function() {
-       
+
            if ($scope.showCueValues[index].countdownTime <= 0)
            {
              countDownStepValue = 10;
-             
+
              $interval.cancel($scope.showCueValues[index].intervalId);
              $scope.showCueValues[index].show = true;
              $scope.buyTimerRunning = false;
              $scope.informationAcquired = true;
              $scope.aquiredInfos.push($scope.showCueValues[index].id);
-             
+
              $scope.currentCueTime = 0;
              $scope.currentCueEndTime = 0;
-             
+
              return;
            }
-           
+
            $scope.showCueValues[index].countdownTime -= 10;
         }, 10);
       }
@@ -231,54 +231,54 @@ angular.module('mouselabApp')
           $scope.buyTimerRunning = false;
           $scope.informationAcquired = true;
           $scope.aquiredInfos.push($scope.showCueValues[index].id);
-          
+
           var acquisitionPattern = determineAcquisitionPattern();
           var timeCost = determineTimeCost(acquisitionPattern, dataService.getCurrentTask());
-          
+
           var localTimeCost = timeCost - $scope.sumOfTimeCosts;
-          
+
           $scope.availableTime -= ($scope.showCueValues[index].countdownTime + localTimeCost);
           //$scope.availableTime -= $scope.showCueValues[index].countdownTime;
-          
+
           $scope.sumOfTimeCosts = timeCost;
       }
     };
-    
-    
+
+
     $scope.chooseShare = function(share) {
       if (!$scope.informationAcquired) { return;  }
       $scope.informationAcquired = false;
-      
+
       var acquiredWeights = 0;
       var localAccuracy   = 0;
       var acquisitionTime = 0;
-      
+
       angular.forEach($scope.cueValues, function(value, key) {
         acquiredWeights += value.weight * $scope.showCueValues[key].show;
         localAccuracy += value.weight * $scope.showCueValues[key].show * getCueScore(key, share);
-        
+
         // Calculate sum of acquired times
-        
+
         acquisitionTime += value.cost * $scope.showCueValues[key].show;
       });
-      
-      
+
+
       localAccuracy /= getMaxLocalAccuracy();
       var localAccuracy2 = localAccuracy < 1 ? 0 : 1;
-      
+
       var trialScore =  Math.round(100 * acquiredWeights * localAccuracy2);
       $scope.currentScore += trialScore;
-      
-    
+
+
       var numberOfAcquisitions = 4;
       for (var indexOfAcquisition = $scope.aquiredInfos.length; indexOfAcquisition < 4; indexOfAcquisition++)
       {
         $scope.aquiredInfos.push(0);
         numberOfAcquisitions -= 1;
       }
-      
+
       var aqcuisitionPattern = determineAcquisitionPattern(numberOfAcquisitions);
-      
+
 
       $scope.finishedTrialData.push({
         number:               $scope.finishedTrials + 1,
@@ -294,11 +294,11 @@ angular.module('mouselabApp')
         acquisitionTime:      acquisitionTime,
         acquisitionOrder:     $scope.aquiredInfos.join(':')
       });
-      
+
       setupNextTrial();
     };
-    
-    
+
+
     function getCueScore(index, share) {
       if (share === 'A')
       {
@@ -309,30 +309,30 @@ angular.module('mouselabApp')
         return 1 - $scope.currentTrial.pattern[index];
       }
     }
-    
+
     function getMaxLocalAccuracy() {
       var accuracy1 = 0;
       var accuracy2 = 0;
-      
+
       angular.forEach($scope.cueValues, function(value, key) {
         accuracy1 += value.weight * $scope.showCueValues[key].show * getCueScore(key, 'A');
         accuracy2 += value.weight * $scope.showCueValues[key].show * getCueScore(key, 'B');
       });
-      
+
       return Math.max(accuracy1, accuracy2);
     }
-    
+
     function determineAcquisitionPattern() {
       var cue1 = $scope.showCueValues[0].isAcquired;
       var cue2 = $scope.showCueValues[1].isAcquired;
       var cue3 = $scope.showCueValues[2].isAcquired;
       var cue4 = $scope.showCueValues[3].isAcquired;
-      
+
       if (!cue1 && !cue2 && !cue3 &&  cue4) { return 15; }
       if (!cue1 && !cue2 &&  cue3 && !cue4) { return 14; }
       if (!cue1 &&  cue2 && !cue3 && !cue4) { return 13; }
       if ( cue1 && !cue2 && !cue3 && !cue4) { return 12; }
-      
+
       if (!cue1 && !cue2 &&  cue3 &&  cue4) { return 11; }
       if (!cue1 &&  cue2 && !cue3 &&  cue4) { return 10; }
       if ( cue1 && !cue2 && !cue3 &&  cue4) { return  9; }
@@ -344,21 +344,21 @@ angular.module('mouselabApp')
       if ( cue1 && !cue2 &&  cue3 &&  cue4) { return  4; }
       if ( cue1 &&  cue2 && !cue3 &&  cue4) { return  3; }
       if ( cue1 &&  cue2 &&  cue3 && !cue4) { return  2; }
-      
+
       if ( cue1 &&  cue2 &&  cue3 &&  cue4) { return  1; }
     }
-    
+
     function determineTimeCost(acquisitionPattern, condition) {
         var timeCosts = {
-          'X_A' : [2330, 3590, 3730, 3930, 4070, 1840, 2050, 2180, 2190, 2320,  960, 310, 440, 190, 0],
-          'X_B' : [3510, 2640, 2790, 2990, 2350, 1620, 1820, 1550, 1560, 1340, 1230, 430, 330, 150, 0],
-          'X_C' : [4510, 3030, 2680, 2890, 2580, 1840, 1570, 1480, 1490, 1430, 1270, 440, 310, 100, 0],
-          
+          'X_A' : [ 830, 2590, 2730, 2930, 3070, 1340, 1550, 1680, 1690, 1820,  460, 310, 440, 190, 0],
+          'X_B' : [2010, 1640, 1790, 1990, 1350, 1120, 1320, 1050, 1060,  840,  730, 430, 330, 150, 0],
+          'X_C' : [3010, 2030, 1680, 1890, 1580, 1340, 1070,  980,  990,  930,  770, 440, 310, 100, 0],
+
           'Y_A' : [2330, 3590, 3730, 3930, 4070, 1840, 2050, 2180, 2190, 2320,  960, 310, 440, 190, 0],
           'Y_B' : [3510, 2640, 2790, 2990, 2350, 1620, 1820, 1550, 1560, 1340, 1230, 430, 330, 150, 0],
           'Y_C' : [4510, 3030, 2680, 2890, 2580, 1840, 1570, 1480, 1490, 1430, 1270, 440, 310, 100, 0]
         };
-        
+
         return timeCosts[condition][acquisitionPattern - 1];
     }
   });
